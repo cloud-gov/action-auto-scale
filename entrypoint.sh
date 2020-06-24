0ebe4e70cf48350b0f5d25c95ebd1fefc86cbb23
Mark Headd <mheadd@gmail.com>
2020-06-24 11:45:52 -0400

    Update entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,14 +21,14 @@ for i in $(echo $STATS | jq '.[] | (.stats.usage.mem /.stats.mem_quota)*100')
 AVG_MEM_UTILIZATION=$(echo "scale=2; $TOTAL / $COUNT" | bc)
 
 # If average utilization is above max threshold, scale up.
-if [ ${AVG_MEM_UTILIZATION/.*} > "$INPUT_APP_MAX_MEM_THRESHOLD" ]; then
+if  (($AVG_MEM_UTILIZATION > "$INPUT_APP_MAX_MEM_THRESHOLD")) ; then
     echo "Average memory utilization across instances is approximately $AVG_MEM_UTILIZATION%."
     echo "Scaling UP"
     cf scale "$INPUT_CF_APP_NAME" -i $((NUM_INSTANCES+INPUT_APP_INSTANCE_INCREMENT))
 fi
 
 # If average utilization is below max threshold, scale down.
-if [ ${AVG_MEM_UTILIZATION/.*} < "$INPUT_APP_MIN_MEM_THRESHOLD" ]; then
+if  (($AVG_MEM_UTILIZATION < "$INPUT_APP_MIN_MEM_THRESHOLD")) ; then
     echo "Average memory utilization across instances is approximately $AVG_MEM_UTILIZATION%."
     if [ $NUM_INSTANCES -gt 1 ]; then
         echo "Scaling DOWN"
@@ -40,7 +40,7 @@ fi
 
 # Display number of instances.
 NEW_INSTANCE_COUNT=$(cf curl "/v2/apps/$GUID/stats" | jq '. | length')
-if [ $NUM_INSTANCES == 1 ]; then
+if [ $NUM_INSTANCES -eq 1 ]; then
     echo "Number of instances remains $NUM_INSTANCES"
 else 
     echo "Number of instances changed from $NUM_INSTANCES to $NEW_INSTANCE_COUNT"
